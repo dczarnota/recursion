12bb4cfa1dcd8c99e223c9b2d9737bbc6d774905
Dominic Czarnota <dczarno@gmail.com>
2014-07-07 20:58:39 -0400

    Implemented stringifyJSON using recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,42 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if(typeof obj === 'string'){
+		return "\"" + obj + "\"";
+	}
+	if (typeof obj === 'boolean'){
+		return "" + obj + "";
+	}
+	if (typeof obj === 'number'){
+		return "" + obj + "";
+	}
+	if (obj === undefined || typeof obj === "function"){
+        return "";
+    }
+    if (obj === null){
+        return "null";
+    }
+	if (Array.isArray(obj)){
+		var arrayResult = "[";
+		for(var i=0; i<obj.length; i++)
+		{
+			if(i>0) arrayResult += ",";
+			arrayResult += stringifyJSON(obj[i]);
+		}
+		return arrayResult + "]";
+	} else {
+		var objKeys = Object.keys(obj);
+		var result = [];
+		var open = "{", close = "}";
+		for(var j=0; j<objKeys.length; j++){
+			var item = objKeys[j];
+			var prop = obj[item];
+			if(prop === undefined || typeof prop === "function")
+				return open + close;
+			var stringify = "\"" + item + "\":";
+			stringify = (typeof prop === "string") ? stringify += "\"" + prop +"\"" : stringify += stringifyJSON(prop);
+			result.push(stringify);
+		}
+	return open + result.join() + close;
+	}
 };
